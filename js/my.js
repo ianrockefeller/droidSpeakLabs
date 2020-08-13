879fe4ed5e015bbf1783636020609b0220ec2b68
Ian Rockefeller <ian.rockefeller@signanthealth.com>
2020-08-12 21:49:56 -0400

    hello world, i need libraries!

---
+++ js/my.js
@@ -0,0 +1,142 @@
+var NUM_SLIDES = 5;
+var currSlideIdx = 0;
+var lastSlideIdx = 0;
+
+function SmoothVerticalScrolling(e, time, where) {
+    var eTop = e.getBoundingClientRect().top;
+    var eAmt = eTop / 100;
+    var curTime = 0;
+    while (curTime <= time) {
+        window.setTimeout(SVS_B, curTime, eAmt, where);
+        curTime += time / 100;
+    }
+}
+
+function SVS_B(eAmt, where) {
+    if(where == "center" || where == "")
+        window.scrollBy(0, eAmt / 2);
+    if (where == "top")
+        window.scrollBy(0, eAmt);
+}
+
+
+$(function() {
+  $('.scrolldown').click(function() {
+    $('.slideBtn')[1].click();
+  });
+
+  $(".slideBtn").click(function(e) {
+    lastSlideIdx = currSlideIdx;
+    currSlideIdx = $(this).index();
+
+    var curr_section = $('.sections>section')[currSlideIdx];
+    var last_section = $('.sections>section')[lastSlideIdx];
+    // curr_section.focus();
+    // SmoothVerticalScrolling(curr_section, 200, 'top')
+    // curr_section.scrollIntoView({behavior: 'smooth', block: 'end'});
+
+    $('html, body').animate({
+      scrollTop: $(curr_section).offset().top
+    }, 300)
+    
+    $('.slideBtn').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    setTimeout(function() {
+      $('.nav-wrapper').removeClass('black').removeClass('white');
+      $('.logo').removeClass('black').removeClass('white').removeClass('blue');
+      $('.hamburger-wrapper').removeClass('black').removeClass('white').removeClass('blue');
+      if($(curr_section).hasClass('white-font')) {
+        $('.nav-wrapper').addClass('white');
+        $('.hamburger-wrapper').addClass('white');
+        $('.logo').addClass('white');
+      } else if($(curr_section).hasClass('black-font')) {
+        $('.nav-wrapper').addClass('black');
+        $('.hamburger-wrapper').addClass('black');
+        $('.logo').addClass('black');
+      } else if($(curr_section).hasClass('blue-font')) {
+        $('.nav-wrapper').addClass('blue');
+        $('.hamburger-wrapper').addClass('blue');
+        $('.logo').addClass('blue');
+      }
+    }, 500);
+
+  });
+
+  $('.anim-typewriter').on('animationiteration webkitAnimationIteration oAnimationIteration MSAnimationIteration',   
+  function(e) {
+    // Word finished typing/deleting
+    if(e.originalEvent.animationName === 'typewriter') {
+      // change the word
+      sIdx++;
+      sIdx %= skills.length;
+      setWord($(this), skills[sIdx])
+    }
+  });
+
+  $('.hamburger').click(function() {
+    // hamburger is active
+    if($('.hamburger').hasClass('is-active')) {
+      scrolling = false;
+      $('.hamburger').removeClass('is-active');
+      $('.menu-wrapper').removeClass('is-active');
+      setTimeout(function() { 
+        if($('.nav-wrapper').hasClass('black')) {
+          $('.logo').removeClass('white');
+          $('.hamburger-wrapper').removeClass('white');
+          $('.logo').addClass('black');
+          $('.hamburger-wrapper').addClass('black');
+        }
+
+        $('.menu-wrapper').toggle(); 
+        
+      }, 300);
+    } else {
+      scrolling = true;
+      $('.menu-wrapper').toggle();
+      setTimeout(function() { 
+        $('.menu-wrapper').addClass('is-active');
+
+        if($('.nav-wrapper').hasClass('black')) { // the slide is black
+          $('.logo').removeClass('black');
+          $('.hamburger-wrapper').removeClass('black');
+          $('.logo').addClass('white');
+          $('.hamburger-wrapper').addClass('white');
+        } 
+
+        $('.hamburger').addClass('is-active');  
+      }, 200)
+    }
+  });
+
+  disableScroll();
+
+  var skills = [
+    'Digital',
+    'Website',
+    // 'Design',
+    'Marketing',
+    'Hosting',
+    'WordPress',
+    'Software',
+    'eCommerce',
+    'SEO',
+    // 'CMS',
+    // 'Shopify',
+    'Web App',
+    // 'Hybrid Mobile',
+    // 'Cutting-edge'
+  ];
+
+  var sIdx = 0;
+
+  setWord($('.tw-line'), skills[sIdx]);
+
+  function setWord(el, skill) {
+    el.html(skill);
+    el.css('animation', 'typewriter 3s steps(' + skill.length + ') 1s infinite normal, blinkTextCursor 500ms steps(' + skill.length + ') infinite normal')
+  } 
+
+  $('.slideBtn')[0].click();
+
+});
---
+++ js/scroll.js
@@ -0,0 +1,117 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1, 32: 1, 33: 1, 34: 1};
+scrolling = false;
+
+function preventDefault(e) {
+  // console.log('prevent default')
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function nextSlide(n) {
+  if(scrolling) {
+    preventDefault();
+    return false;
+  }
+
+  scrolling = true;
+  setTimeout(function() {
+    scrolling = false;
+  }, 1000);
+
+  var nextIdx = currSlideIdx + n;
+
+  if(nextIdx <= (NUM_SLIDES - 1) && nextIdx >= 0) {
+    $('.slideBtn')[nextIdx].click();
+    currSlideIdx = nextIdx;
+  }
+}
+
+function preventDefaultForWheel(e) {
+  if(scrolling) {
+    // console.log('prevent default for wheel STOPPED')
+    preventDefault(e);
+    return false;
+  }
+  // console.log('prevent default for wheel GO')
+
+  if(e.deltaY < 0) {
+    // up
+    // console.log('UP nextslide')
+    nextSlide(-1)
+  } else {
+    // down
+    // console.log('DOWN nextslide')
+    nextSlide(1)
+  }
+
+  preventDefault(e);
+  return false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  // console.log('prevent default for scroll keys')
+  switch(e.keyCode) {
+    case 35:
+    case 36:
+      preventDefault(e);
+      break;
+    case 38:
+    case 33:
+      nextSlide(-1);  
+      preventDefault(e);
+      break;
+    case 40:
+    case 32:
+    case 34:
+      nextSlide(1); 
+      preventDefault(e);
+      break;
+    default:
+      return;
+  }
+  return false;
+}
+
+function disableScroll() {
+  if (window.addEventListener) { // older FF
+    window.addEventListener('DOMMouseScroll', preventDefaultForWheel, false);
+    window.addEventListener('mousewheel', preventDefaultForWheel, false);    
+  }
+  window.onmousewheel = document.onmousewheel = preventDefaultForWheel; // older browsers, IE
+  window.onwheel = preventDefaultForWheel; 
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+
+  document.addEventListener('wheel', preventDefaultForWheel, { passive: false });
+  // document.addEventListener('DOMMouseScroll', preventDefaultForWheel, {passive: false});
+  // document.addEventListener('mousewheel', preventDefaultForWheel, {passive: false});
+}
+
+// function enableScroll() {
+//     if (window.removeEventListener)
+//         window.removeEventListener('DOMMouseScroll', preventDefault, false);
+//     window.onmousewheel = document.onmousewheel = null; 
+//     window.onwheel = null; 
+//     window.ontouchmove = null;  
+//     document.onkeydown = null;  
+// }
+
+var ts;
+$(document).bind('touchstart', function (e){
+   ts = e.originalEvent.touches[0].clientY;
+});
+
+$(document).bind('touchend', function (e){
+   var te = e.originalEvent.changedTouches[0].clientY;
+   if(ts > te+5){
+      // slide_down();
+      nextSlide(1);
+   }else if(ts < te-5){
+      // slide_up();
+      nextSlide(-1);
+   }
+});
